449f6f97cd58c8f9925849d61cd570013f663fd2
Bryan <71095749+bryanless@users.noreply.github.com>
2023-01-09 12:11:54 +0700

    Move font to core module

--- Yume/App/YumeApp.swift
+++ Yume/App/YumeApp.swift
@@ -15,6 +15,10 @@ import SwiftUI
 struct YumeApp: App {
   @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
+  init() {
+    UIFont.loadFonts()
+  }
+
   var body: some Scene {
     let injection = Injection.init()
 
