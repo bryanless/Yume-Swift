d8f468479bcea9a8564f1891ecaaf6565b668d8b
Bryan <71095749+bryanless@users.noreply.github.com>
2023-01-09 10:37:10 +0700

    Add contains words to string extension

--- Modules/Core/Sources/Core/Extension/String+Ext.swift
+++ Modules/Core/Sources/Core/Extension/String+Ext.swift
@@ -59,4 +59,28 @@ extension String {
   public func snakeCaseToTitleCase() -> String {
     return self.replacingOccurrences(of: "_", with: " ").capitalized
   }
+
+  public func containsWords(_ words: String, caseInsensitive: Bool = true) -> Bool {
+    // Check if it should be case insensitive
+    let firstString = caseInsensitive ? self.lowercased() : self
+    let secondString = caseInsensitive ? words.lowercased() : words
+
+    // Trim whitespaces, split by dash, joined by whitespace, remove punctuation, split by whitespace
+    let firstStringCharacters = firstString
+      .trimmingCharacters(in: .whitespacesAndNewlines)
+      .split(separator: "-")
+      .joined(separator: " ")
+      .trimmingCharacters(in: .punctuationCharacters)
+      .split(separator: " ")
+    let secondStringCharacters = secondString.filter { !$0.isPunctuation }
+      .trimmingCharacters(in: .whitespacesAndNewlines)
+      .split(separator: "-")
+      .joined(separator: " ")
+      .trimmingCharacters(in: .punctuationCharacters)
+      .split(separator: " ")
+
+    return secondStringCharacters.allSatisfy { secondCharacter in
+      firstStringCharacters.contains(where: { $0.hasPrefix(secondCharacter)  })
+    }
+  }
 }
